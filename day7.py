9b200491b54a5deb5b5196e73143bb48f741938d
n-j-p <10066183+n-j-p@users.noreply.github.com>
2025-12-07 17:10:07 +1100

    day 7 part 2 slow

--- day7.py
+++ day7.py
@@ -1,6 +1,6 @@
 def string_replace(s, i, c = '|'):
     return s[:i] + c + s[(i+1):]
-def splitter(data):
+def splitter(data, VERBOSE=False):
     i = data[0].index('S')
 
     beams = set((i,))
@@ -20,11 +20,45 @@ def splitter(data):
             else:
                 newrow = string_replace(newrow, i)
                 newbeams.add(i)
-        print(f'{r:3d}', data[r], '  ', newrow)#string_replace(sample_input[r],i))
+        if VERBOSE: print(f'{r:3d}', data[r], '  ', newrow)#string_replace(sample_input[r],i))
         beams = set(newbeams)
     return c
 
-if __name__ == '__main__':
-    sample_input = open('day7_sample_input.txt').read().split('\n')[:-1]
+import itertools as it
+import tqdm
+def quantum(data, VERBOSE=False):
+    i = data[0].index('S')
+    from collections import defaultdict
+    timelines = defaultdict(list)
+    timelines[i] = ['']
 
-    print(splitter(sample_input))
+
+
+    for r in tqdm.tqdm(range(1, len(data))):
+        newtimelines = defaultdict(list)
+        newrow = data[r]
+        for i in timelines.keys():
+            if newrow[i] == '^':
+                newtimelines[i-1] += [x + 'L' for x in timelines[i]]
+                newtimelines[i+1] += [x + 'R' for x in timelines[i]]
+                # raise NotImplementedError
+            else:
+                newtimelines[i] += list(timelines[i])
+        if VERBOSE: print(r, ':', newtimelines)
+        timelines = dict(newtimelines)
+
+    c = 0
+
+    for x in it.chain.from_iterable(timelines.values()):
+        c += 1
+
+    return c
+
+
+
+
+if __name__ == '__main__':
+    # sample_input = open('day7_sample_input.txt').read().split('\n')[:-1]
+    actual_input = open('c:/temp/day7_input.txt').read().split('\n')[:-1]
+    print(f'Part 1 answer is {splitter(actual_input)}')
+    print(f'Part 2 answer is {quantum(actual_input)}')
