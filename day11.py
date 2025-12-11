6d2d01147da74faf4f8e0037c83d658173d20f8b
n-j-p <10066183+n-j-p@users.noreply.github.com>
2025-12-12 07:34:31 +1100

    started traverse_part2

--- day11.py
+++ day11.py
@@ -3,6 +3,7 @@ import pdb
 import matplotlib.pyplot as plt
 import tqdm
 sample_data = open('day11_sample_input.txt', 'r').read().split('\n')[:-1]
+sample_data2 = open('day11_sample_input_part2.txt', 'r').read().split('\n')[:-1]
 actual_data = open('c:/temp/day11_input.txt', 'r').read().split('\n')[:-1]
 def graphise(data):
     g = networkx.DiGraph()
@@ -32,7 +33,12 @@ def part2(data):
         if 'dac' in p and 'fft' in p:
             c += 1
     return c
+r'''
+Let's do a DFS to begin with.
 
+
+
+'''
 class DiGraph():
     def __init__(self, connexions):
         self.connexions = connexions
@@ -45,7 +51,7 @@ class DiGraph():
             self.tree[node] = []
             for n2 in rs[1:]:
                 self.tree[node].append(n2)
-    def traverse(self, root, dest):
+    def traverse_part1(self, root, dest):
         assert root in self.tree
         cur = root
         
@@ -62,14 +68,103 @@ class DiGraph():
             path.append(cur)
             if cur == dest:
                 # pdb.set_trace()
-                return path
-            seen.add(cur)
+                yield tuple(path)
+
+                # Backtrack
+                path.pop(-1)
+
+                last = path[-1]
+                while path_ix[last]+1 == len(self.tree[last]):
+                    path_ix.pop(last)
+                    path.pop(-1)
+                    try:
+                        last = path[-1]
+                    except IndexError:
+                        return
+
+                path_ix[last] += 1
+                # pdb.set_trace()
+                cur = self.tree[last][path_ix[last]]
+                path.append(cur)
+
+
+            # if cur in path_ix:
+                
+            # seen.add(cur)
+            # pdb.set_trace()
+            if cur in path_ix:
+                raise NotImplementedError('Cycle detected?')
+            path_ix[cur] = 0
             try:
-                nxt += self.tree[cur]
+                
+                nxt = [self.tree[cur][path_ix[cur]]]
             except KeyError: # Sink node
                 continue
             
+    def traverse_part2(self, root, dest):
+        assert root in self.tree
+        cur = root
+        
+        path_ix = {}
 
+        seen = set([])
+
+        nxt = [cur,]
+        path = []
+        while len(nxt) > 0:
+            cur = nxt.pop()
+            if cur in seen:
+                continue
+            path.append(cur)
+            if cur == dest:
+                # pdb.set_trace()
+                yield tuple(path)
+
+                # Backtrack
+                path.pop(-1)
+
+                last = path[-1]
+                while path_ix[last]+1 == len(self.tree[last]):
+                    path_ix.pop(last)
+                    path.pop(-1)
+                    try:
+                        last = path[-1]
+                    except IndexError:
+                        return
+
+                path_ix[last] += 1
+                # pdb.set_trace()
+                cur = self.tree[last][path_ix[last]]
+                path.append(cur)
+
+
+            # if cur in path_ix:
+                
+            # seen.add(cur)
+            # pdb.set_trace()
+            if cur in path_ix:
+                raise NotImplementedError('Cycle detected?')
+            path_ix[cur] = 0
+            try:
+                
+                nxt = [self.tree[cur][path_ix[cur]]]
+            except KeyError: # Sink node
+                # Probably better if we treat this as before, but...
+                path_ix.pop(cur)
+                path.pop(-1)
+                last = path[-1]
+                while path_ix[last]+1 == len(self.tree[last]):
+                    path_ix.pop(last)
+                    path.pop(-1)
+                    try:
+                        last = path[-1]
+                    except IndexError:
+                        return
+                # pdb.set_trace()
+                path_ix[last] += 1
+                nxt = [self.tree[last][path_ix[last]],]
+
+                pass
 
 
 
